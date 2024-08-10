7b5dd34ba06ee41a902811c0198e1d31c9360ea0
milosz.hoc <miloszhoc@gmail.com>
2024-08-10 15:44:18 +0200

    .

--- dd.py
+++ dd.py
@@ -16,19 +16,20 @@ print(start_time)
 # model = GPT4All("Meta-Llama-3-8B-Instruct.Q4_0.gguf", device='cuda')  # downloads / loads a 4.66GB LLM
 model = GPT4All("Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf", device='cuda')
 with model.chat_session():
-    print(model.generate("""
+    resp = model.generate("""
     UWAGA BIAŁYSTOK, NOWY RADIOWÓZ !\n\nSkoda Octavia III Drogówki z Białegostoku (Woj. Podlaskie)\nSilnik: 1,8 Turbo / 180 KM\nWyposażenie: 2 kamery polcam\nNadzorowany teren: Miasto Białystok i Powiat białostocki. Droga S8, Drogi Krajowe numer 8, 19, 65 oraz Drogi Wojewódzkie numer: 676, 678.\nBIA 28075\n\n#Policja #Radiowóz #Białystok #Videorejestrator #Skoda #Octavia3 #Drogówka #Podlaskie #BIA28075 #nieoznakowaneradiowozy Pokaż mniej
 
-    z powyższych informacji wyciągnij polską nazwę województwa i miasta, marka i model samochodu, aktualny polski numer rejestracyjny pojazdu skadający sie z cyfr i liter, jeśli są, to poprzednie numery rejestracyjne i polskie numery dróg po których pojazd się porusza. wyniki zapisz w formacie json o strukturze:
-    {voivodeship : nazwa województwa (string),
-    city: miasto (string),
-    car_info: marka i model samochodu (string),
-    current_licence_plate_number: aktualny polski numer rejestracyjny pojazdu skadający sie z cyfr i liter (string)
-    old_license_plates: poprzednie numery rejestracyjne (list),
-    road_numbers: polskie numery dróg po których pojazd się porusza (list),
+    From the above information presented in Polish, extract the Polish name of the province and city, make and model of car, current Polish vehicle registration number consisting of numbers and letters. If there are, previous registration numbers and Polish numbers of roads on which the vehicle moves. save the results in json format with the structure:
+    {voivodeship : province name (string),
+    city: city (string),
+    car_info: car make and model (string),
+    current_licence_plate_number: current Polish vehicle registration number consisting of numbers and letters (string)
+    old_license_plates: previous registration numbers (list),
+    road_numbers: Polish numbers of roads on which the vehicle moves (list),
     }
-    jeśli brakuje danych, to pozostaw puste pole. Zwróć tylko json.
-    """, max_tokens=1024))
+    if data is missing, leave blank. Return only the json.
+    """, max_tokens=1024)
+    print(resp)
 end_time = int(time.time())
 
-print(f'{end_time - start_time}sek.')
+print(f'execution time {end_time - start_time}sek.')
