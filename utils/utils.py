bf1d62d1fdf322b6a9582373066de91eb07b469e
miloszhoc <miloszhoc@gmail.com>
2024-08-18 19:21:14 +0200

    .

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,10 @@
+import asyncio
+import json
 import re
+import time
 from urllib.parse import unquote
+
+import boto3
 
 
 def extract_data_from_urls(urls):
@@ -21,3 +26,15 @@ def extract_data_from_urls(urls):
                                  'color': color,
                                  'city': country}
     return car_data
+
+
+def add_timestamp(input_str, ext='jpg'):
+    return f'{input_str}_{int(time.time())}.{ext}'
+
+
+async def upload_file_to_s3(semaphore, bucket_name, local_file_path):
+    async with semaphore:
+        remote_file_path = f"pictures/{local_file_path.split('/')[-1]}"
+        s3 = boto3.resource('s3')
+        s3.Bucket(bucket_name).upload_file(local_file_path, remote_file_path)
+        return bucket_name, remote_file_path
