028a6be6ca624fe083f7919f284e0ac49ca6beba
miloszhoc <miloszhoc@gmail.com>
2024-09-24 21:57:39 +0200

    .

--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -27,21 +27,20 @@ def upload_to_s3(file_name, bucket, object_name=None):
 
 
 def lambda_handler(event, context):
-    source_json_content = get_file_content_from_s3(event['source_filename'])
+    item = json.loads(event['source_item'])
 
     genai.configure(api_key=os.environ["GEMINI_API_KEY"])
     model = genai.GenerativeModel('gemini-1.5-flash',
                                   generation_config={"response_mime_type": "application/json"})
 
     with tempfile.NamedTemporaryFile(mode='w+', suffix='.json', delete=False) as temp_file:
-        for item in source_json_content[:2]:
-            try:
-                item['llm_extracted']
-            except KeyError:
-                prompt = f"""
-                        {item['description']}
+        prompt = f"""
+                    {item['description']}
             
-                        From the above information presented in Polish, extract the Polish name of the province and city, make and model of car, current Polish vehicle registration number consisting of numbers and letters. If there are, previous registration numbers and Polish numbers of roads on which the vehicle moves. save the results in json format with the structure:
+                        From the above information presented in Polish, extract the Polish name of the province and city, 
+                        make and model of car, current Polish vehicle registration number consisting of numbers and letters. 
+                        If there are, previous registration numbers and Polish numbers of roads on which the vehicle moves. 
+                        save the results in json format with the structure:
                         {{voivodeship : province name (string),
                         city: city (string),
                         car_info: car make and model (string),
@@ -49,22 +48,22 @@ def lambda_handler(event, context):
                         old_license_plates: previous registration numbers (list),
                         road_numbers: Polish numbers of roads on which the vehicle moves (list),
                         }}
-                        if data is missing, leave blank. Return only the json structure and nothing else.
+                        If data is missing, leave blank. Return only the json structure and nothing else.
                         """
-            else:
-                print('NOTHING TO DO')
-            response = model.generate_content(prompt)
-            print(response.text)
-            item['llm_extracted'] = json.loads(response.text)
-        time.sleep(1)
+        response = model.generate_content(prompt)
+        print(response.text)
+        item['llm_extracted'] = json.loads(response.text)
+        destination_json_content = get_file_content_from_s3('data.json')
+        print('destination_json_content before extend', destination_json_content)
+        destination_json_content.append(item)
+        print('destination_json_content after extend', destination_json_content)
 
-        destination_json_content = get_file_content_from_s3(event['destination_filename'])
-        destination_json_content.extend(source_json_content)
         json.dump(destination_json_content, temp_file, indent=3, ensure_ascii=False)
+        temp_file.seek(0)
 
         temp_file_name = temp_file.name
 
-    upload_to_s3(temp_file_name, 'cops-detector-pictures', event['destination_filename'])
+        upload_to_s3(temp_file_name, 'cops-detector-pictures', 'data.json')
 
     return {'statusCode': 200,
             'body': response.text}
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import json
+import random
 import re
 import time
 from urllib.parse import unquote
@@ -6,6 +7,20 @@ import pathlib
 import boto3
 
 TEST_DATA_PATH = pathlib.Path('test_data/group_photos/')
+
+
+class FunctionError(Exception):
+    pass
+
+
+def get_file_content_from_s3(filename):
+    s3 = boto3.client('s3')
+    stream = s3.get_object(Bucket='cops-detector-pictures', Key=filename)['Body'].iter_lines()
+    content = []
+    for i in stream:
+        content.append(i.decode('utf-8'))
+    json_content = json.loads(''.join(content))
+    return json_content
 
 
 def extract_data_from_urls(urls):
@@ -53,3 +68,11 @@ async def upload_file_to_s3(semaphore, bucket_name, local_file_path):
         s3 = boto3.resource('s3')
         s3.Bucket(bucket_name).upload_file(local_file_path, remote_file_path)
         return bucket_name, remote_file_path
+
+
+def invoke_lamda(function_name: str, payload: dict, invocation_type: str = 'RequestResponse') -> str:
+    payload = json.dumps(payload)
+    client = boto3.client('lambda')
+    response = client.invoke(FunctionName=function_name, InvocationType=invocation_type, Payload=payload)
+    return response['ResponseMetadata']
+
